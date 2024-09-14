c5806be74404d60367365a906302f1ae6e245549
Ondřej Hložek <hlozek.o1@gmail.com>
2024-09-14 23:15:47 +0200

    feat: added utility method

---
+++ src/main/java/cz/inqool/tennis_club/exception/ExceptionUtils.java
@@ -0,0 +1,27 @@
+package cz.inqool.tennis_club.exception;
+
+import java.util.Objects;
+
+public final class ExceptionUtils {
+
+    private ExceptionUtils() {
+        throw new UnsupportedOperationException("Utility class cannot be instantiated");
+    }
+
+    /**
+     * Retrieves the root cause of an exception.
+     *
+     * @param throwable the exception to inspect
+     * @return the root cause of the exception
+     */
+    public static Throwable getRootCause(Throwable throwable) {
+        Objects.requireNonNull(throwable);
+        Throwable rootCause = throwable;
+        while (rootCause.getCause() != null) {
+            rootCause = rootCause.getCause();
+        }
+
+        return rootCause;
+    }
+
+}
