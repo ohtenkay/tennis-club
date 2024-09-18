99afacbe0bd5e4e33e3e203d6aade30f9383e1db
Ondřej Hložek <hlozek.o1@gmail.com>
2024-09-19 00:45:14 +0200

    fix: added truncating of the interval and docs

--- src/main/java/cz/inqool/tennis_club/util/DateTimeUtils.java
+++ src/main/java/cz/inqool/tennis_club/util/DateTimeUtils.java
@@ -14,12 +14,22 @@ public final class DateTimeUtils {
         throw new UnsupportedOperationException("Utility class cannot be instantiated");
     }
 
+    /**
+     * Returns the closest interval mark to the given local date time. Also
+     * truncates the seconds and nanoseconds.
+     *
+     * @param localDateTime    the local date time to find the closest interval mark
+     *                         for
+     * @param intervalDuration the duration of the interval in minutes
+     * @param boundary         the boundary of the interval
+     * @return the closest interval mark as LocalDateTime
+     */
     public static LocalDateTime getClosestIntervalMark(LocalDateTime localDateTime, int intervalDuration,
             IntervalBoundary boundary) {
         val minute = localDateTime.getMinute();
         val remainder = minute % intervalDuration;
         val closestIntervalMark = minute - remainder + (boundary == IntervalBoundary.UPPER ? intervalDuration : 0);
 
-        return localDateTime.withMinute(closestIntervalMark);
+        return localDateTime.withMinute(closestIntervalMark).withSecond(0).withNano(0);
     }
 }
